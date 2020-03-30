894f2d1638a7b27917645d6728b9009902198ab4
Wilton Zhan <WiltonZhan@outlook.com>
2020-03-30 21:59:44 +0800

    Add: 190 (reverse bits).

---
+++ src/main/java/com/WiltonZhan/leetcode/l190ReverseBits/Solution.java
@@ -0,0 +1,8 @@
+package com.WiltonZhan.leetcode.l190ReverseBits;
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+}

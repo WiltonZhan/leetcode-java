7f9787596df9b6bff48a6ffc86791351365dbf47
Wilton Zhan <WiltonZhan@outlook.com>
2020-06-12 08:27:18 +0800

    Update: 15 (three sum).

--- src/main/java/com/WiltonZhan/leetcode/l15ThreeSum/Solution.java
+++ src/main/java/com/WiltonZhan/leetcode/l15ThreeSum/Solution.java
@@ -2,11 +2,61 @@ package com.WiltonZhan.leetcode.l15ThreeSum;
 
 import java.util.*;
 
+/*
+15. 三数之和
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+
+
+示例：
+
+给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+ */
 public class Solution {
+    /**
+     *  使用双指针
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int sum = -nums[i];
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] > sum) {
+                    r--;
+                } else if (nums[l] + nums[r] < sum) {
+                    l++;
+                } else {
+                    // 去重
+                    while (l + 1 < r && nums[l + 1] == nums[l]) l++;
+                    while (r - 1 > l && nums[r - 1] == nums[r]) r--;
+
+                    result.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
+                }
+            }
+            while (i + 1 < nums.length && nums[i + 1] == nums[i]) i++;
+        }
+
+        return result;
+    }
+
     /**
      *  使用查找表
      */
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSum_lookup_table(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null || nums.length < 3) {
             return result;
--- src/test/java/com/WiltonZhan/leetcode/l15ThreeSum/SolutionTest.java
+++ src/test/java/com/WiltonZhan/leetcode/l15ThreeSum/SolutionTest.java
@@ -2,6 +2,7 @@ package com.WiltonZhan.leetcode.l15ThreeSum;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +12,12 @@ class SolutionTest {
 
     @Test
     void threeSum() {
-        List<List<Integer>> result1 = solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
-        assertEquals(2, result1.size());
+        List<List<Integer>> result = new ArrayList<>();
+        result = solution.threeSum(new int[]{0,0,0,0,0});
+        assertEquals(1, result.size());
+        result = solution.threeSum(new int[]{-2,0,1,1,2});
+        assertEquals(2, result.size());
+        result = solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        assertEquals(2, result.size());
     }
 }

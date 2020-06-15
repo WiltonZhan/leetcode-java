ef75e24239e2b3c01884bf82c9018abfbf7f1054
Wilton Zhan <WiltonZhan@outlook.com>
2020-06-15 08:20:01 +0800

    Update: 14 (longest common prefix).

--- src/main/java/com/WiltonZhan/leetcode/l14LongestCommonPrefix/Solution.java
+++ src/main/java/com/WiltonZhan/leetcode/l14LongestCommonPrefix/Solution.java
@@ -1,5 +1,21 @@
 package com.WiltonZhan.leetcode.l14LongestCommonPrefix;
 
+/*
+14. 最长公共前缀
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+ */
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) {

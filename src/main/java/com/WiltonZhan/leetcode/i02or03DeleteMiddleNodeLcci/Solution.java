a193a9664fc877126777e4f5551e5054312e7ebd
Wilton Zhan <WiltonZhan@outlook.com>
2020-06-24 08:35:23 +0800

    Add: interview questions 02.03. (delete middle node lcci).

---
+++ src/main/java/com/WiltonZhan/leetcode/i02or03DeleteMiddleNodeLcci/Solution.java
@@ -0,0 +1,27 @@
+package com.WiltonZhan.leetcode.i02or03DeleteMiddleNodeLcci;
+
+/*
+面试题 02.03. 删除中间节点
+实现一种算法，删除单向链表中间的某个节点（即不是第一个或最后一个节点），假定你只能访问该节点。
+
+示例：
+
+输入：单向链表a->b->c->d->e->f中的节点c
+结果：不返回任何数据，但该链表变为a->b->d->e->f
+ */
+public class Solution {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {val = x;}
+    }
+
+    /**
+     * 用下一个节点的值替换当前节点的值，然后删除下一个节点
+     */
+    public void deleteNode(ListNode node) {
+        ListNode next = node.next;
+        node.val = next.val;
+        node.next = next.next;
+    }
+}

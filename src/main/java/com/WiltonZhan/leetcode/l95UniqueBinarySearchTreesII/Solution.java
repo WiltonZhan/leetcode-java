c584b97eac78110e987c8e3f3c494cbbc9fa2a05
Wilton Zhan <WiltonZhan@outlook.com>
2020-07-21 08:16:15 +0800

    Add：95 (unique binary search trees II).

---
+++ src/main/java/com/WiltonZhan/leetcode/l95UniqueBinarySearchTreesII/Solution.java
@@ -0,0 +1,82 @@
+package com.WiltonZhan.leetcode.l95UniqueBinarySearchTreesII;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 95. 不同的二叉搜索树 II
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：3
+ * 输出：
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * 解释：
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ *
+ *
+ * 提示：
+ *
+ * 0 <= n <= 8
+ */
+public class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {this.val = val;}
+        TreeNode (int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n <= 0) {
+            return new LinkedList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // 枚举所有的根结点
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            for (TreeNode leftTree: leftTrees) {
+                for (TreeNode rightTree: rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = leftTree;
+                    root.right = rightTree;
+                    allTrees.add(root);
+                }
+            }
+        }
+
+        return allTrees;
+    }
+}
